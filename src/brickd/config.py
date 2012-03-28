f46932a5dcde72d92e5054b256c2f2e037441cd7
Olaf Lüke <olaf@tinkerforge.com>
2012-03-28 21:00:28 +0200

    change version to 1.0.2

--- src/brickd/config.py
+++ src/brickd/config.py
@@ -4,9 +4,9 @@
 
 import logging
 
-LOGGING_LEVEL = logging.ERROR
+LOGGING_LEVEL = logging.WARN
 LOGGING_FORMAT = "%(asctime)s <%(levelname)s> <%(filename)s:%(lineno)s> %(message)s"
 LOGGING_DATEFMT = "%H:%M:%S"
 PORT = 4223
 
-BRICKD_VERSION = "1.0.1"
+BRICKD_VERSION = "1.0.2"
