07560de97f0dd54c7bb79d25ee8f034decd244c0
batti <batti@workstation.(none)>
2012-04-08 09:29:00 +0200

    remove debug output

--- src/build_data/macos/data/install_script.sh
+++ src/build_data/macos/data/install_script.sh
@@ -3,7 +3,6 @@
 # programm nach /usr/libexec/brickd
 
 cd $1
-ls > /tmp/lala
 cp com.tinkerforge.brickd.plist /System/Library/LaunchDaemons/
 cp -r brickd.app /usr/libexec/
 launchctl load /System/Library/LaunchDaemons/com.tinkerforge.brickd.plist
