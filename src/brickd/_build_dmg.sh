4b1997b59494914d67d4e2555bb16585ecb3ed25
Matthias Bolte <matthias@tinkerforge.com>
2012-06-28 19:41:25 +0200

    Improve version handling for Mac OS installer creation

--- src/brickd/_build_dmg.sh
+++ src/brickd/_build_dmg.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-rm Brickd.dmg
-hdiutil create -fs HFS+ -volname Brickd -srcfolder macos_build Brickd.dmg
-
+dot_version=`grep BRICKD_VERSION config.py | sed -e 's/BRICKD_VERSION = "\(.*\)"/\1/'`
+underscore_version=`printf %s $dot_version | sed -e 's/\./_/g'`
+dmg=brickd_macos_$underscore_version.dmg
+rm $dmg
+hdiutil create -fs HFS+ -volname "Brickd $dot_version" -srcfolder macos_build $dmg
