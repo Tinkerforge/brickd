8fc4502cb59da2d07b2660b404c1383e70bb5bbd
Matthias Bolte <matthias@tinkerforge.com>
2012-07-02 18:15:09 +0200

    macos: Remove space from .dmg volume name to make installer work again

--- src/brickd/_build_dmg.sh
+++ src/brickd/_build_dmg.sh
@@ -3,4 +3,4 @@ dot_version=`grep BRICKD_VERSION config.py | sed -e 's/BRICKD_VERSION = "\(.*\)"
 underscore_version=`printf %s $dot_version | sed -e 's/\./_/g'`
 dmg=brickd_macos_$underscore_version.dmg
 rm $dmg
-hdiutil create -fs HFS+ -volname "Brickd $dot_version" -srcfolder macos_build $dmg
+hdiutil create -fs HFS+ -volname "Brickd-$dot_version" -srcfolder macos_build $dmg
