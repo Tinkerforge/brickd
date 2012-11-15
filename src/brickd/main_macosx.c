531cba306cb62410a429c610f817c34488e585cc
Matthias Bolte <matthias@tinkerforge.com>
2012-11-15 18:36:14 +0100

    Update Makefile for Mac OS X and add main_macosx,c

--- src/brickd/main_macosx.c
+++ src/brickd/main_macosx.c
@@ -18,3 +18,96 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
+
+#include <string.h>
+
+#include "event.h"
+#include "log.h"
+#include "network.h"
+//#include "iokit.h"
+#include "usb.h"
+#include "version.h"
+
+#define LOG_CATEGORY LOG_CATEGORY_OTHER
+
+static void print_usage(const char *binary) {
+	printf("Usage: %s [--help|--version]\n", binary);
+}
+
+int main(int argc, char **argv) {
+	int exit_code = 2;
+
+	if (argc > 2) {
+		print_usage(argv[0]);
+
+		return 1;
+	}
+
+	if (argc > 1) {
+		if (strcmp(argv[1], "--help") == 0) {
+			print_usage(argv[0]);
+
+			return 0;
+		} else if (strcmp(argv[1], "--version") == 0) {
+			printf("%s\n", VERSION_STRING);
+
+			return 0;
+		} else {
+			print_usage(argv[0]);
+
+			return 1;
+		}
+	}
+
+	log_init();
+
+	// FIXME: read config
+	log_set_level(LOG_CATEGORY_EVENT, LOG_LEVEL_DEBUG);
+	log_set_level(LOG_CATEGORY_USB, LOG_LEVEL_DEBUG);
+	log_set_level(LOG_CATEGORY_NETWORK, LOG_LEVEL_DEBUG);
+	log_set_level(LOG_CATEGORY_HOTPLUG, LOG_LEVEL_DEBUG);
+	log_set_level(LOG_CATEGORY_OTHER, LOG_LEVEL_DEBUG);
+
+	log_info("Brick Daemon %s started", VERSION_STRING);
+
+	if (event_init() < 0) {
+		goto error_event;
+	}
+
+	if (usb_init() < 0) {
+		goto error_usb;
+	}
+
+	/*if (iokit_init() < 0) {
+		goto error_iokit;
+	}*/
+
+	if (network_init() < 0) {
+		goto error_network;
+	}
+
+	if (event_run() < 0) {
+		goto error_run;
+	}
+
+	exit_code = 0;
+
+error_run:
+	network_exit();
+
+error_network:
+	//iokit_exit();
+
+//error_iokit:
+	usb_exit();
+
+error_usb:
+	event_exit();
+
+error_event:
+	log_info("Brick Daemon %s stopped", VERSION_STRING);
+
+	log_exit();
+
+	return exit_code;
+}
