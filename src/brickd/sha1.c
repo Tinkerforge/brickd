d41a8b034d9f812299bbee16eb6b308c9a7d36db
Matthias Bolte <matthias@tinkerforge.com>
2014-03-14 19:46:54 +0100

    sha1: Merge sha1_pad into sha1_final and remove inline uncommon assignment

--- src/brickd/sha1.c
+++ src/brickd/sha1.c
@@ -24,8 +24,7 @@
 
 // blk0() and blk() perform the initial expand. blk0() deals with host endianess
 #define blk0(i) (block[i] = htonl(block[i]))
-#define blk(i) (block[i&15] = rol(block[(i+13)&15]^block[(i+8)&15] \
-    ^block[(i+2)&15]^block[i&15],1))
+#define blk(i) (block[i&15] = rol(block[(i+13)&15]^block[(i+8)&15]^block[(i+2)&15]^block[i&15],1))
 
 // (R0+R1), R2, R3, R4 are the different operations (rounds) used in SHA1
 #define R0(v,w,x,y,z,i) z+=((w&(x^y))^y)+blk0(i)+0x5A827999+rol(v,5);w=rol(w,30);
@@ -54,16 +53,19 @@ static uint32_t sha1_transform(SHA1 *sha1, const uint8_t buffer[SHA1_BLOCK_LENGT
 	R0(c,d,e,a,b, 8); R0(b,c,d,e,a, 9); R0(a,b,c,d,e,10); R0(e,a,b,c,d,11);
 	R0(d,e,a,b,c,12); R0(c,d,e,a,b,13); R0(b,c,d,e,a,14); R0(a,b,c,d,e,15);
 	R1(e,a,b,c,d,16); R1(d,e,a,b,c,17); R1(c,d,e,a,b,18); R1(b,c,d,e,a,19);
+
 	R2(a,b,c,d,e,20); R2(e,a,b,c,d,21); R2(d,e,a,b,c,22); R2(c,d,e,a,b,23);
 	R2(b,c,d,e,a,24); R2(a,b,c,d,e,25); R2(e,a,b,c,d,26); R2(d,e,a,b,c,27);
 	R2(c,d,e,a,b,28); R2(b,c,d,e,a,29); R2(a,b,c,d,e,30); R2(e,a,b,c,d,31);
 	R2(d,e,a,b,c,32); R2(c,d,e,a,b,33); R2(b,c,d,e,a,34); R2(a,b,c,d,e,35);
 	R2(e,a,b,c,d,36); R2(d,e,a,b,c,37); R2(c,d,e,a,b,38); R2(b,c,d,e,a,39);
+
 	R3(a,b,c,d,e,40); R3(e,a,b,c,d,41); R3(d,e,a,b,c,42); R3(c,d,e,a,b,43);
 	R3(b,c,d,e,a,44); R3(a,b,c,d,e,45); R3(e,a,b,c,d,46); R3(d,e,a,b,c,47);
 	R3(c,d,e,a,b,48); R3(b,c,d,e,a,49); R3(a,b,c,d,e,50); R3(e,a,b,c,d,51);
 	R3(d,e,a,b,c,52); R3(c,d,e,a,b,53); R3(b,c,d,e,a,54); R3(a,b,c,d,e,55);
 	R3(e,a,b,c,d,56); R3(d,e,a,b,c,57); R3(c,d,e,a,b,58); R3(b,c,d,e,a,59);
+
 	R4(a,b,c,d,e,60); R4(e,a,b,c,d,61); R4(d,e,a,b,c,62); R4(c,d,e,a,b,63);
 	R4(b,c,d,e,a,64); R4(a,b,c,d,e,65); R4(e,a,b,c,d,66); R4(d,e,a,b,c,67);
 	R4(c,d,e,a,b,68); R4(b,c,d,e,a,69); R4(a,b,c,d,e,70); R4(e,a,b,c,d,71);
@@ -83,24 +85,6 @@ static uint32_t sha1_transform(SHA1 *sha1, const uint8_t buffer[SHA1_BLOCK_LENGT
 	return a; // return a to avoid dead-store warning from clang static analyzer
 }
 
-static void sha1_pad(SHA1 *sha1) {
-	uint8_t final_count[8];
-	uint32_t i;
-
-	for (i = 0; i < 8; i++) {
-		// endian independent
-		final_count[i] = (uint8_t)((sha1->count >> ((7 - (i & 7)) * 8)) & 255);
-	}
-
-	sha1_update(sha1, (uint8_t *)"\200", 1);
-
-	while ((sha1->count & 504) != 448) {
-		sha1_update(sha1, (uint8_t *)"\0", 1);
-	}
-
-	sha1_update(sha1, final_count, 8); // should cause a sha1_transform()
-}
-
 void sha1_init(SHA1 *sha1) {
 	sha1->state[0] = 0x67452301;
 	sha1->state[1] = 0xEFCDAB89;
@@ -117,11 +101,13 @@ void sha1_update(SHA1 *sha1, const uint8_t *data, size_t length) {
 	sha1->count += (length << 3);
 
 	if ((j + length) > 63) {
-		memcpy(&sha1->buffer[j], data, (i = 64 - j));
+		i = 64 - j;
+
+		memcpy(&sha1->buffer[j], data, i);
 		sha1_transform(sha1, sha1->buffer);
 
 		for (; i + 63 < length; i += 64) {
-			sha1_transform(sha1, (uint8_t *)&data[i]);
+			sha1_transform(sha1, &data[i]);
 		}
 
 		j = 0;
@@ -134,8 +120,20 @@ void sha1_update(SHA1 *sha1, const uint8_t *data, size_t length) {
 
 void sha1_final(SHA1 *sha1, uint8_t digest[SHA1_DIGEST_LENGTH]) {
 	uint32_t i;
+	uint8_t count[8];
 
-	sha1_pad(sha1);
+	for (i = 0; i < 8; i++) {
+		// this is endian independent
+		count[i] = (uint8_t)((sha1->count >> ((7 - (i & 7)) * 8)) & 255);
+	}
+
+	sha1_update(sha1, (uint8_t *)"\200", 1);
+
+	while ((sha1->count & 504) != 448) {
+		sha1_update(sha1, (uint8_t *)"\0", 1);
+	}
+
+	sha1_update(sha1, count, 8);
 
 	for (i = 0; i < SHA1_DIGEST_LENGTH; i++) {
 		digest[i] = (uint8_t)((sha1->state[i >> 2] >> ((3 - (i & 3)) * 8)) & 255);
