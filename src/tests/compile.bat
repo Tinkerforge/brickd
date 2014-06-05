ac8388bd4d14fce66a8406d94a67cc6cc355f0ed
Matthias Bolte <matthias@tinkerforge.com>
2014-06-05 10:49:29 +0200

    windows: Compile tests in release mode

--- src/tests/compile.bat
+++ src/tests/compile.bat
@@ -8,14 +8,14 @@
  goto done
 )
 
-rem @set CC=cl /nologo /c /MD /O2 /W4 /wd4200 /wd4214 /FI..\brickd\fixes_msvc.h^
+@set CC=cl /nologo /c /MD /O2 /W4 /wd4200 /wd4214 /FI..\brickd\fixes_msvc.h^
  /FIbool_msvc.h /DWIN32_LEAN_AND_MEAN /DNDEBUG
-@set CC=cl /nologo /c /MD /Zi /EHsc /Oy- /Ob0 /W4 /wd4200 /wd4214^
- /FI..\brickd\fixes_msvc.h /FIbool_msvc.h /DWIN32_LEAN_AND_MEAN /DDEBUG
+rem @set CC=cl /nologo /c /MD /Zi /EHsc /Oy- /Ob0 /W4 /wd4200 /wd4214^
+rem  /FI..\brickd\fixes_msvc.h /FIbool_msvc.h /DWIN32_LEAN_AND_MEAN /DDEBUG
 @set MC=mc
 @set RC=rc /dWIN32 /r
-rem @set LD=link /nologo /opt:ref /opt:icf /release
-@set LD=link /nologo /debug
+@set LD=link /nologo /opt:ref /opt:icf /release
+rem @set LD=link /nologo /debug
 @set AR=link /lib /nologo
 @set MT=mt /nologo
 
