e640ee74c4ca49c71ac2b74f1d99a9612e7a7c89
Matthias Bolte <matthias@tinkerforge.com>
2014-07-31 18:50:40 +0200

    windows: Change logviewer view menu items to radio style

--- src/build_data/windows/logviewer/logviewer.c
+++ src/build_data/windows/logviewer/logviewer.c
@@ -85,6 +85,7 @@ enum {
 static void create_menu(void) {
 	HMENU menu = CreateMenu();
 	HMENU file_menu = CreatePopupMenu();
+	MENUITEMINFO menu_item_info;
 
 	_view_menu = CreatePopupMenu();
 
@@ -95,6 +96,22 @@ static void create_menu(void) {
 	AppendMenu(menu, MF_STRING | MF_POPUP, (UINT)_view_menu, "&View");
 	AppendMenu(_view_menu, MF_STRING, ID_VIEW_EVENT, "Windows &Event Log");
 	AppendMenu(_view_menu, MF_STRING, ID_VIEW_DEBUG, "Live &Debug Log");
+
+	memset(&menu_item_info, 0, sizeof(menu_item_info));
+
+	menu_item_info.cbSize = sizeof(menu_item_info);
+	menu_item_info.fMask = MIIM_FTYPE;
+	menu_item_info.fType = MFT_STRING | MFT_RADIOCHECK;
+
+	SetMenuItemInfo(_view_menu, ID_VIEW_EVENT, FALSE, &menu_item_info);
+
+	memset(&menu_item_info, 0, sizeof(menu_item_info));
+
+	menu_item_info.cbSize = sizeof(menu_item_info);
+	menu_item_info.fMask = MIIM_FTYPE;
+	menu_item_info.fType = MFT_STRING | MFT_RADIOCHECK;
+
+	SetMenuItemInfo(_view_menu, ID_VIEW_DEBUG, FALSE, &menu_item_info);
 
 	SetMenu(_hwnd, menu);
 }
--- src/build_data/windows/logviewer/version.h
+++ src/build_data/windows/logviewer/version.h
@@ -24,7 +24,7 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 1
-#define VERSION_RELEASE 0
+#define VERSION_RELEASE 1
 
 #define INT_TO_STRING_(x) #x
 #define INT_TO_STRING(x) INT_TO_STRING_(x)
