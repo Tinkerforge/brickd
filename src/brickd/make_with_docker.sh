27b64ccc684d38a1c1f7c8b5f537b64aed277af3
Olaf Lüke <olaf@tinkerforge.com>
2018-07-10 18:12:28 +0200

    Add script to build brickd with docker

---
+++ src/brickd/make_with_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ROOT_DIR=`/bin/pwd`
+DAEMONLIB_DIR=$(realpath $ROOT_DIR/../daemonlib)
+
+if [ ! -d "$ROOT_DIR/../daemonlib" ]; then \
+	echo "Could not find daemonlib. Please symlink daemonlib into brickd/src/ folder."; \
+	exit 1; \
+fi
+
+if command -v docker >/dev/null 2>&1 ; then
+	if [ $(/usr/bin/docker images -q build_environment_c) ]; then
+		echo "Using docker image to build.";
+		docker run \
+		-v $ROOT_DIR/../:/$ROOT_DIR/../ -u $(id -u):$(id -g) \
+		-v $DAEMONLIB_DIR/:$DAEMONLIB_DIR/: -u $(id -u):$(id -g) \
+		-ti build_environment_c /bin/bash \
+		-c "cd $ROOT_DIR ; make "$@""; \
+	else
+		echo "No docker image found.";
+	fi
+else
+	echo "Docker not found";
+fi
