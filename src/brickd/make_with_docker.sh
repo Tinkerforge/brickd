2b8dc71739d50daab6ebb14e99f5158776e593b0
Matthias Bolte <matthias@tinkerforge.com>
2018-09-03 12:03:30 +0200

    docker: Only enable interactive mode when running in terminal

--- src/brickd/make_with_docker.sh
+++ src/brickd/make_with_docker.sh
@@ -3,17 +3,23 @@
 ROOT_DIR=`/bin/pwd`
 DAEMONLIB_DIR=$(realpath $ROOT_DIR/../daemonlib)
 
+if [ -t 0 ]; then
+	DOCKER_FLAGS="-it"
+else
+	DOCKER_FLAGS=
+fi
+
 if command -v docker >/dev/null 2>&1 ; then
 	if [ $(/usr/bin/docker images -q tinkerforge/build_environment_c) ]; then
-		echo "Using docker image to build.";
-		docker run -it \
+		echo "Using docker image to build."
+		docker run $DOCKER_FLAGS \
 		-v $ROOT_DIR/../:/$ROOT_DIR/../ -u $(id -u):$(id -g) \
 		-v $DAEMONLIB_DIR/:$DAEMONLIB_DIR/: -u $(id -u):$(id -g) \
 		tinkerforge/build_environment_c /bin/bash \
-		-c "cd $ROOT_DIR ; make "$@""; \
+		-c "cd $ROOT_DIR ; make "$@""
 	else
-		echo "No docker image found.";
+		echo "No docker image found."
 	fi
 else
-	echo "Docker not found";
+	echo "Docker not found."
 fi
