24917ea981cdb779c7148225cfa20ba5b6dca4c5
Matthias Bolte <matthias@tinkerforge.com>
2020-05-14 14:31:58 +0200

    alpine: Add docker based build script

---
+++ src/build_data/alpine/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rm -r packages
+mkdir packages
+
+docker image rm tinkerforge/build_brickd_alpine
+docker build -t tinkerforge/build_brickd_alpine .
+
+docker run --rm -v ${PWD}/brickd:/brickd -v ${PWD}/packages:/packages \
+  tinkerforge/build_brickd_alpine /bin/sh -c \
+  'chown user:user /brickd /packages; su user -c "cd /brickd; ls -la; abuild -r; cp -r /home/user/packages/* /packages"'
+
+docker image rm tinkerforge/build_brickd_alpine
