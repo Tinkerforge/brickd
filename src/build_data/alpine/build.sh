c9c06a707e4b283a6ea01e765348106f7fadde04
Matthias Bolte <matthias@tinkerforge.com>
2020-05-14 14:37:11 +0200

    alpine: Remove unnecessary ls command

--- src/build_data/alpine/build.sh
+++ src/build_data/alpine/build.sh
@@ -8,6 +8,6 @@ docker build -t tinkerforge/build_brickd_alpine .
 
 docker run --rm -v ${PWD}/brickd:/brickd -v ${PWD}/packages:/packages \
   tinkerforge/build_brickd_alpine /bin/sh -c \
-  'chown user:user /brickd /packages; su user -c "cd /brickd; ls -la; abuild -r; cp -r /home/user/packages/* /packages"'
+  'chown user:user /brickd /packages; su user -c "cd /brickd; abuild -r; cp -r /home/user/packages/* /packages"'
 
 docker image rm tinkerforge/build_brickd_alpine
