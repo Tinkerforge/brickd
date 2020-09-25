020485b06440e4aa65fee56ffe4412e89ed4cf8b
Matthias Bolte <matthias@tinkerforge.com>
2020-09-25 17:03:15 +0200

    linux: Document docker setup for Debian package build

--- src/build_data/linux/installer/docker/build.sh
+++ src/build_data/linux/installer/docker/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -ex
 
+# sudo apt-get install qemu binfmt-support qemu-user-static
+# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
 for architecture in amd64 i386 arm32v7 arm64v8; do
     docker build --no-cache --build-arg ARCHITECTURE=${architecture} -t tinkerforge/builder-brickd-debian-${architecture}:1.0.0 .
 done
