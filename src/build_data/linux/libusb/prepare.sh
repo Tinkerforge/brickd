005a9548c71249ece9fb5cee65425df7ca5a8599
Matthias Bolte <matthias@tinkerforge.com>
2021-04-27 19:42:24 +0200

    Remove -r from rm when removing a single file

--- src/build_data/linux/libusb/prepare.sh
+++ src/build_data/linux/libusb/prepare.sh
@@ -4,7 +4,7 @@ version=1.0.24.11609
 commit=1001cb5558cf6679af7bce3114bba1d3bb7b6f7f
 
 rm -rf libusb-src
-rm -rf libusb-${commit}.zip
+rm -f libusb-${commit}.zip
 curl -LOJ https://github.com/libusb/libusb/archive/${commit}.zip
 unzip libusb-${commit}.zip
 mv libusb-${commit} libusb-src
