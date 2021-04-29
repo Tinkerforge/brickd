2c7bc8921da3981e739b363d6def486eb64021f0
Matthias Bolte <matthias@tinkerforge.com>
2021-04-29 14:05:13 +0200

    android: Adapt to libusb event handling changes

--- src/brickd/usb_android.c
+++ src/brickd/usb_android.c
@@ -20,83 +20,128 @@
  */
 
 #include <jni.h>
-#include <errno.h>
+#include <libusb.h>
 
 #include <daemonlib/event.h>
 #include <daemonlib/log.h>
-#include <daemonlib/pipe.h>
 #include <daemonlib/utils.h>
 
 #include "usb.h"
 
 static LogSource _log_source = LOG_SOURCE_INITIALIZER;
 
-static Pipe _hotplug_pipe;
+static void usb_handle_events_internal(void *opaque) {
+	libusb_context *context = opaque;
+	struct timeval tv;
+	int rc;
 
-static void usb_forward_hotplug(void *opaque) {
-	uint8_t byte;
+	tv.tv_sec = 0;
+	tv.tv_usec = 0;
 
+	rc = libusb_handle_events_timeout(context, &tv);
+
+	if (rc < 0) {
+		log_error("Could not handle USB events: %s (%d)", usb_get_error_name(rc), rc);
+	}
+}
+
+static void LIBUSB_CALL usb_add_pollfd(int fd, short events, void *opaque) {
+	libusb_context *context = opaque;
+
+	log_event_debug("Got told to add libusb pollfd (handle: %d, events: %d)", fd, events);
+
+	// FIXME: handle error?
+	event_add_source(fd, EVENT_SOURCE_TYPE_USB, "usb-poll", events,
+	                 usb_handle_events_internal, context);
+}
+
+static void LIBUSB_CALL usb_remove_pollfd(int fd, void *opaque) {
 	(void)opaque;
 
-	if (pipe_read(&_hotplug_pipe, &byte, sizeof(byte)) < 0) {
-		log_error("Could not read from hotplug pipe: %s (%d)",
-		          get_errno_name(errno), errno);
+	log_event_debug("Got told to remove libusb pollfd (handle: %d)", fd);
 
-		return;
+	event_remove_source(fd, EVENT_SOURCE_TYPE_USB);
+}
+
+int usb_init_platform(libusb_context *context) {
+	int phase = 0;
+	const struct libusb_pollfd **pollfds = NULL;
+	const struct libusb_pollfd **pollfd;
+	const struct libusb_pollfd **last_added_pollfd = NULL;
+
+	// get pollfds from libusb context
+	pollfds = libusb_get_pollfds(context);
+
+	if (pollfds == NULL) {
+		log_error("Could not get pollfds from libusb context");
+
+		goto cleanup;
 	}
 
-	log_debug("Starting USB device scan, triggered by hotplug");
+	for (pollfd = pollfds; *pollfd != NULL; ++pollfd) {
+		if (event_add_source((*pollfd)->fd, EVENT_SOURCE_TYPE_USB, "usb-poll",
+		                     (*pollfd)->events, usb_handle_events_internal, context) < 0) {
+			goto cleanup;
+		}
 
-	usb_rescan();
-}
-
-int usb_init_platform(void) {
-	return 0;
-}
-
-void usb_exit_platform(void) {
-}
-
-int usb_init_hotplug(libusb_context *context) {
-	(void)context;
-
-	// create hotplug pipe
-	if (pipe_create(&_hotplug_pipe, PIPE_FLAG_NON_BLOCKING_READ) < 0) {
-		log_error("Could not create hotplug pipe: %s (%d)",
-		          get_errno_name(errno), errno);
-
-		return -1;
+		last_added_pollfd = pollfd;
+		phase = 1;
 	}
 
-	if (event_add_source(_hotplug_pipe.base.read_handle, EVENT_SOURCE_TYPE_GENERIC,
-	                     "hotplug", EVENT_READ, usb_forward_hotplug, NULL) < 0) {
-		pipe_destroy(&_hotplug_pipe);
+	// register pollfd notifiers
+	libusb_set_pollfd_notifiers(context, usb_add_pollfd, usb_remove_pollfd, context);
 
-		return -1;
+	phase = 2;
+
+cleanup:
+	switch (phase) { // no breaks, all cases fall through intentionally
+	case 1:
+		for (pollfd = pollfds; pollfd != last_added_pollfd; ++pollfd) {
+			event_remove_source((*pollfd)->fd, EVENT_SOURCE_TYPE_USB);
+		}
+
+		if (last_added_pollfd != NULL) {
+			event_remove_source((*last_added_pollfd)->fd, EVENT_SOURCE_TYPE_USB);
+		}
+
+		// fall through
+
+	default:
+		break;
 	}
 
-	return 0;
+	libusb_free_pollfds(pollfds);
+
+	return phase == 2 ? 0 : -1;
 }
 
-void usb_exit_hotplug(libusb_context *context) {
-	(void)context;
+void usb_exit_platform(libusb_context *context) {
+	const struct libusb_pollfd **pollfds = NULL;
+	const struct libusb_pollfd **pollfd;
 
-	event_remove_source(_hotplug_pipe.base.read_handle, EVENT_SOURCE_TYPE_GENERIC);
-	pipe_destroy(&_hotplug_pipe);
+	libusb_set_pollfd_notifiers(context, NULL, NULL, NULL);
+
+	pollfds = libusb_get_pollfds(context);
+
+	if (pollfds == NULL) {
+		log_error("Could not get pollfds from libusb context");
+	} else {
+		for (pollfd = pollfds; *pollfd != NULL; ++pollfd) {
+			event_remove_source((*pollfd)->fd, EVENT_SOURCE_TYPE_USB);
+		}
+
+		libusb_free_pollfds(pollfds);
+	}
 }
 
-bool usb_has_hotplug(void) {
-	return true;
+void usb_handle_events_platform(libusb_context *context) {
+	usb_handle_events_internal(context);
 }
 
 JNIEXPORT void JNICALL
 Java_com_tinkerforge_brickd_MainService_hotplug(JNIEnv *env, jobject this) {
-	uint8_t byte = 0;
-
+	(void)env;
 	(void)this;
 
-	if (pipe_write(&_hotplug_pipe, &byte, sizeof(byte)) < 0) {
-		log_error("Could not write to hotplug pipe: %s (%d)",
-		          get_errno_name(errno), errno);
-	}
+	usb_handle_hotplug();
 }
