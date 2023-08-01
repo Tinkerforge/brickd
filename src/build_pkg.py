aae8f02f07cb4cb707950191a758a3522edab4e1
Matthias Bolte <matthias@tinkerforge.com>
2023-08-01 15:10:48 +0200

    macos: Switch from deprecated altool to notarytool

--- src/build_pkg.py
+++ src/build_pkg.py
@@ -33,7 +33,7 @@ import sys
 import os
 import shutil
 import subprocess
-import plistlib
+import json
 import time
 import re
 import argparse
@@ -155,31 +155,40 @@ def build_macos_pkg():
         print('notarize app')
         zip_path = os.path.join(dist_path, 'brickd.app.zip')
         system('ditto -c -k --keepParent "{0}" "{1}"'.format(app_path, zip_path))
-        output = subprocess.check_output(['xcrun', 'altool', '--notarize-app', '--primary-bundle-id', 'com.tinkerforge.brickd', '--username', 'olaf@tinkerforge.com', '--password', '@keychain:Notarization', '--output-format', 'xml', '--file', zip_path])
-        plist = plistlib.loads(output)
+        output = subprocess.check_output(['xcrun', 'notarytool', 'submit', zip_path, '--output-format', 'json', '--keychain-profile', 'notary-tinkerforge'])
+        notarization_submit = json.loads(output)
 
         try:
-            request_uuid = plist['notarization-upload']['RequestUUID']
+            request_id = notarization_submit['id']
         except:
             print('error: notarization output does not contain expected fields')
             pprint.pprint(output)
-            pprint.pprint(plist)
+            pprint.pprint(notarization_submit)
             sys.exit(1)
 
-        print('notarize app request uuid', request_uuid)
+        print('notarize app request id', request_id)
         notarization_info = None
+        notarization_status = None
 
         while True:
             try:
-                output = subprocess.check_output(['xcrun', 'altool', '--notarization-info', request_uuid, '--username', 'olaf@tinkerforge.com', '--password', '@keychain:Notarization', '--output-format', 'xml'])
+                output = subprocess.check_output(['xcrun', 'notarytool', 'info', request_id, '--output-format', 'json', '--keychain-profile', 'notary-tinkerforge'])
             except subprocess.CalledProcessError as e:
                 print('warning: notarize query failed, retrying', e)
                 time.sleep(1)
                 continue
 
-            notarization_info = plistlib.loads(output)['notarization-info']
+            notarization_info = json.loads(output)
 
-            if notarization_info['Status'] != 'in progress':
+            try:
+                notarization_status = notarization_info['status']
+            except:
+                print('error: notarization output does not contain expected fields')
+                pprint.pprint(output)
+                pprint.pprint(notarization_info)
+                sys.exit(1)
+
+            if notarization_status != 'In Progress':
                 break
 
             print('waiting for app notarization to complete ...')
@@ -187,7 +196,7 @@ def build_macos_pkg():
 
         print('notarization app info', notarization_info)
 
-        if notarization_info['Status'] != 'success':
+        if notarization_status != 'Accepted':
             print('error: notarization app failed')
             sys.exit(1)
 
