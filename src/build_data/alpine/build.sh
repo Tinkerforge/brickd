353d58b5edca10e4909338c265840456267f6604
Matthias Bolte <matthias@tinkerforge.com>
2024-07-02 10:40:27 +0200

    alpine: Unify docker image name format

--- src/build_data/alpine/build.sh
+++ src/build_data/alpine/build.sh
@@ -6,15 +6,15 @@ else
     docker_host="-H tcp://$1"
 fi
 
-docker ${docker_host} image rm -f tinkerforge/build_brickd_alpine || true
-docker ${docker_host} build -t tinkerforge/build_brickd_alpine .
+docker ${docker_host} image rm -f tinkerforge/builder-brickd-alpine || true
+docker ${docker_host} build -t tinkerforge/builder-brickd-alpine .
 
-container=$(docker ${docker_host} create tinkerforge/build_brickd_alpine)
+container=$(docker ${docker_host} create tinkerforge/builder-brickd-alpine)
 docker ${docker_host} cp ${container}:/home/user/packages .
 docker ${docker_host} cp ${container}:/brickd/APKBUILD brickd/APKBUILD.updated
 
 docker ${docker_host} rm -v $container
-docker ${docker_host} image rm -f tinkerforge/build_brickd_alpine
+docker ${docker_host} image rm -f tinkerforge/builder-brickd-alpine
 
 mkdir -p output
 
